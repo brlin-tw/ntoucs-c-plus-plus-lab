08365475084be39d8cd7c7b6f8b9a0a7ec4b8425
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-02-27 04:37:34 +0800

    完成步驟６

--- Lab_1_2/GNU_environment/Source_code/sort.cpp
+++ Lab_1_2/GNU_environment/Source_code/sort.cpp
@@ -14,10 +14,35 @@
  **************************************************************************/
 /*排序演算法header*/
 #include "Sorting_algorithm.h"
+#include <assert.h>
+
+/*checkSortedInt函式：確定int排序演算法沒有出錯
+    auguments:
+      sorted_data：應該要被排序的int資料陣列
+      size：陣列大小
+    回傳數值：
+      -1：排序有問題
+      0：排序正常*/
+short checkSortedInt(int sorted_data[], unsigned size);
 
 // function that sorts an array
 void sort(int a[], int size)
 {
   mergeSort(a, 0, size - 1);
+  assert(checkSortedInt(a, size) == 0);
+  /*假設排序沒有問題*/
+
   return;
+}
+
+short checkSortedInt(int sorted_data[], unsigned size)
+{
+  unsigned i;
+
+  for(i = 0; i < size - 1; i++){
+    if(sorted_data[i] > sorted_data[i + 1]){
+      return -1;/*排序異常*/
+    }
+  }
+  return 0;/*排序正常*/
 }
--- Lab_1_2/GNU_environment/Source_code/sort.h
+++ Lab_1_2/GNU_environment/Source_code/sort.h
@@ -1,6 +1,7 @@
 #ifndef INCLUDED_SORT_H
   #define INCLUDED_SORT_H
 
-  void sort(int[], int);
+void sort(int[], int);
+
 
 #endif // INCLUDED_SORT_H
