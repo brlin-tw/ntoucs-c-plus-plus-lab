4ae446f616eefeb0e770dc2e4c7a80b379e08c73
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-02-27 13:51:57 +0800

    更新

--- Lab_1_2/GNU_environment/Source_code/main.cpp
+++ Lab_1_2/GNU_environment/Source_code/main.cpp
@@ -19,7 +19,7 @@
  * development, research, and testing of the theories and programs        *
  * to determine their effectiveness. The authors and publisher make       *
  * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
+ * programs or to the documentation contained in thesek books. The authors*
  * and publisher shall not be liable in any event for incidental or       *
  * consequential damages in connection with, or arising out of, the       *
  * furnishing, performance, or use of these programs.                     *
