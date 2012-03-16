054243971a481c1cb42e8973d00f32912c6140b2
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-03-17 05:33:15 +0800

    2.4初步建立

---
+++ Lab_2_4/GNU_toolchain/Source_code/Project_specific_configurations/Debug.h
@@ -0,0 +1,11 @@
+﻿#ifndef DEBUG_H_INCLUDED
+  #define DEBUG_H_INCLUDED
+  /*////////環境設定(Environment Settings)////////*/
+  /*是否顯示偵錯訊息（０為否）？*/
+  #define SHOW_DEBUG_MESSAGE 1
+
+  /*是否build跟偵錯有關的程式碼？*/
+  #define DEBUG
+  /*#define NDEBUG*/
+
+#endif // SHOW_DEBUG_MESSAGE_H_INCLUDED
--- Lab_2_4/GNU_toolchain/Source_code/main.cpp
+++ Lab_2_4/GNU_toolchain/Source_code/main.cpp
@@ -27,6 +27,8 @@
 /*////////環境設定(Environment Settings)////////*/
 
 /*////////程式所include的標頭檔(Included Headers)////////*/
+/*我們需要使用暫停函式*/
+#include "pauseProgram/Pause_program.h"
 
 /*////////常數與巨集(Constants & Macros)以及其他#define指令////////*/
 
@@ -42,6 +44,14 @@
 /*--------------主要程式碼(Main Code)--------------*/
 int main()
 {
+
+/*用來重新開始程式的label*/
+restart_program:
+
+  /*暫停程式*/
+  if(pauseProgram() == 1){
+    goto restart_program;
+  }
 
   /*正常結束程式*/
   return 0;
