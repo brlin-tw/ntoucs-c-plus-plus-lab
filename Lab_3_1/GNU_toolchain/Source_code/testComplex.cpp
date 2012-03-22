f33b00a60421d6c2921f745454dc74cba7628348
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-03-23 06:48:55 +0800

    完成３-１步驟１０

--- Lab_3_1/GNU_toolchain/Source_code/testComplex.cpp
+++ Lab_3_1/GNU_toolchain/Source_code/testComplex.cpp
@@ -1,4 +1,4 @@
-﻿/*程式框架(Program Framework)
+﻿/*testComplex.cpp
 -----------------------------------
 更新紀錄 | Changelog
   Changelog is now stored on GitHub
@@ -28,6 +28,14 @@
 /*////////環境設定(Environment Settings)////////*/
 
 /*////////程式所include的標頭檔(Included Headers)////////*/
+/*We need complex number class*/
+#include "Complex.h"
+
+/*We need pauseProgram function*/
+#include "pauseProgram/Pause_program.h"
+
+/*We need console output method*/
+#include <iostream>
 
 /*////////常數與巨集(Constants & Macros)以及其他#define指令////////*/
 
@@ -44,6 +52,18 @@
 int main()
 {
 
+restart_program:
 
+  //進行複數類別元件測試
+  if(!CComplex::unitTest()){
+    cout << "元件測試未通過！軟體無法繼續運行。" << endl
+         << "請保留程式輸出之所有訊息以提交程式缺陷報告供軟體開發者處理這個問題。" << endl
+         << "提交報告網頁地址：https://github.com/Vdragon/NTOU_CPP_Lab_Opensource_Project/issues" << endl;
+  }
+
+/*end_program:*/
+  if(pauseProgram() == 1){
+    goto restart_program;
+  }
   return 0;
 }
