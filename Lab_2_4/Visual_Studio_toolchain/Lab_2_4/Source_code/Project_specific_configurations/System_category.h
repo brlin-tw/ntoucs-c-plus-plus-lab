cb3f87da0044b9ee7cb9fd6c2ca7cb12896d9730
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-03-26 05:13:52 +0800

    2-4：完成移植Visual Studio toolchain

---
+++ Lab_2_4/Visual_Studio_toolchain/Lab_2_4/Source_code/Project_specific_configurations/System_category.h
@@ -0,0 +1,6 @@
+﻿#ifndef SYSTEM_CATEGORY_H_INCLUDED
+  #define SYSTEM_CATEGORY_H_INCLUDED
+  /*編譯ＯＳ種類：
+  Windows console = 0, Linux console = 1, Symbian console = 2, PSP console = 3*/
+  #define SYSTEM_CATEGORY 0
+#endif /* SYSTEM_CATEGORY_H_INCLUDED*/
---
+++ Lab_2_4/Visual_Studio_toolchain/Lab_2_4/Source_code/main.h
@@ -0,0 +1,23 @@
+﻿/*
+ * main.h
+ *
+ *  Created on: 2012/3/17
+ *      Author: vdragon
+ */
+
+#ifndef MAIN_H_INCLUDED
+  #define MAIN_H_INCLUDED
+  /*使用std namespace的identifier*/
+  using namespace std;
+
+  /*DataRecord資料結構*/
+  typedef struct dataRecord{
+    /*行號*/
+    int lineNumber;
+    /*數值*/
+    int value;
+
+    bool operator==(const struct dataRecord &y);
+  }DataRecord;
+
+#endif /* MAIN_H_INCLUDED */
