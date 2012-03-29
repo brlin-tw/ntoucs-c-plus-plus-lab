c1da3b46f749e8826c35304d4a8d1a8328b77b23
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-03-30 00:24:23 +0800

    4-2移植Visual Studio完成

---
+++ Lab_4_2/Visual_Studio_2008_toolchain/Lab_4_2/Source_code/Complex_number_abstract_data_structure/Complex.h
@@ -0,0 +1,81 @@
+﻿#ifndef COMPLEX_H_INCLUDED
+  #define COMPLEX_H_INCLUDED
+  /* 如果浮點數小於這個常數就會被視為等於零
+   * 注意：設成太小的數值可能導致問題。*/
+  #define MAX_ZERO_LIMIT 0.0000001
+
+  /* 標示目前執行位置的標籤*/
+  #define COMPLEX_NUMBER_DIVISION_TAG "【複數除法模組】"
+  #define UNIT_TEST_TAG "【元件測試模組】"
+
+  /**/
+  #include <iostream>
+  using namespace std;
+
+
+  /*複數物件類別*/
+  class CComplex
+  {
+  public:
+    CComplex(void);
+
+    virtual ~CComplex(void);
+
+    /*計算模塊*/
+    /*method to assign value to a CComplex*/
+    void setValue(double real, double imaginary);
+    /* an method to copy an 存在的複數到此複數物件中*/
+    void setValue(CComplex &source);
+    /*method to add another CComplex to this CComplex*/
+    void add(CComplex target);
+    /*method to subtract another CComplex to this CComplex*/
+    void subtract(CComplex target);
+    /*method to multiply another CComplex to this CComplex*/
+    void multiply(CComplex target);
+    /*method to divide another CComplex to this CComplex*/
+    bool divide(CComplex divider);
+    /*method to test if both complex number is equal */
+    bool isEqual(CComplex target, double max_zero_limit);
+    /*an inline method to check if complex number is 0,
+     * originally used for divide-by-0 checking*/
+    inline bool isZero();
+    /*一個inline的method用來將一個複數轉型成其共軛型式*/
+    inline void toConjugate();
+    /* an inline method to check if complex number is real,
+     * originally used for print() format check*/
+    inline bool isReal();
+
+    /*取值模組*/
+    /*method to get this CComplex's real fraction*/
+    double getReal();
+    /*method to get this CComplex's imaginary fraction*/
+    double getImaginary();
+    /*method to get the length of the CComplex*/
+    double getMagnitude();
+
+    /*輸出輸入模組*/
+    /* method to print complex number in standard form to standard output.
+     * this method is DEPRECATED, use print2Stream() instead.*/
+    void print();
+    /* 將物件印到output stream的method
+     * 此method為print()的generalized版本*/
+    void print2Stream(ostream &out);
+
+    /*測試模組*/
+    /*a method to test CComplex class*/
+    static bool unitTest();
+
+  private:
+    /*複數的實數部份*/
+    double m_real;
+    /*複數的虛數部份*/
+    double m_imaginary;
+  };
+
+  /* overload ostream::operator<< 的 複數insertion operator */
+  ostream &operator<<(ostream &os, CComplex &rhs);
+
+  /*步驟七*/
+  ostream *operator<<(ostream *output, CComplex source);
+#endif
+
---
+++ Lab_4_2/Visual_Studio_2008_toolchain/Lab_4_2/Source_code/Test_program.cpp
@@ -0,0 +1,106 @@
+﻿/*Test_program.cpp
+-----------------------------------
+更新紀錄 | Changelog
+  Changelog is now stored on GitHub
+已知問題 | Known Issues
+  Known issues is now stored on GitHub
+待辦事項 | Todos
+  Todo is now stored on GitHub
+著作權宣告 | Copyright notice
+  Copyright 2012 林博仁(Henry Lin, pika1021@gmail.com)
+智慧財產授權條款：
+  Test_program.cpp is part of Lab_4_2
+  Lab_4_2 is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Lab_4_2 is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with Lab_4_2.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*--------------程式碼開始(Code Started)--------------*/
+/*--------------前期處理器指令(Preprocessor Directive)--------------*/
+/*////////程式所include之函式庫的標頭檔(Included Library Headers)////////*/
+/**/
+#include <cstdlib>
+
+/**/
+#include "pauseProgram/Pause_program.h"
+
+/*complex number*/
+#include "Complex_number_abstract_data_structure/Complex.h"
+
+/**/
+#include <fstream>
+
+/*////////常數與巨集(Constants & Macros)////////*/
+
+/*////////其他前期處理器指令(Other Preprocessor Directives////////*/
+
+/*--------------全域宣告與定義(Global Declaration & Definition)--------------*/
+/*////////Classes、資料結構(Data Structures)、type definitions跟enumerations////////*/
+
+/*////////函式雛型(Function Prototypes)////////*/
+
+/*////////全域變數(Global Variables)////////*/
+
+/*--------------主要程式碼(Main Code)--------------*/
+using namespace std;
+
+int main()
+{
+/*用來重新運行程式的label*/
+restart_program:
+
+  /*unit test*/
+  CComplex::unitTest();
+
+  /*步驟五*/
+  {
+    CComplex x;
+
+    x.setValue(3, 4);
+
+    ofstream file("outputfile.txt", ios::out);
+
+    file << x; // 在檔案裡寫入 3 + 4 i
+  }
+
+
+  /*步驟六*/
+  {
+    CComplex x, y;
+
+    x.setValue(3, 4); y.setValue(5, 6);
+
+    operator<<(operator<<(operator<<(operator<<(operator<<(cout, '('), x) , ")*(") , y), ')') << endl;
+
+  }
+
+  /*步驟七*/
+  {
+    CComplex x1, x2;
+
+    x1.setValue(3.22, -4.19);
+    x2.setValue(-3.1415, 200);
+
+    operator<<(&cout, x1);
+
+    *operator<<(&cout, x1) << endl;
+    *operator<<(operator<<(&cout, x1), x2) << endl;
+
+  }
+  /*暫停程式運行（於main函式中）*/
+  if(pauseProgram() == 1){
+    goto restart_program;
+  }
+
+  /**/
+  return EXIT_SUCCESS;
+}
