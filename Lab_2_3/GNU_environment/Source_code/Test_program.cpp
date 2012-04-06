0f67fbdb17cacff916f00ecf400a1772a0b141c9
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-04-07 03:19:42 +0800

    更新Lab2-3：完成Valgrind Memory leak檢查工具的實作

---
+++ Lab_2_3/GNU_environment/Source_code/Test_program.cpp
@@ -0,0 +1,19 @@
+﻿#include <cstdio>
+#include <malloc.h>
+
+
+int main()
+{
+
+    int *ptr;
+    int i;
+
+    
+    ptr = new int[100];
+    for (i=0; i<100; i++)
+        ptr[i] = i;
+    delete ptr;
+    /*free(ptr);*/
+    
+    return 0;
+}
