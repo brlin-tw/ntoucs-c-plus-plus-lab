526dfc6fbbf845114d3006b34605132766e62327
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-05-17 10:30:44 +0800

    【實習7.1】加入專案檔案

---
+++ Lab_7_1/GNU_toolchain/Source_code/Test.cpp
@@ -0,0 +1,61 @@
+/*Test.cpp
+-----------------------------------
+更新紀錄 | Changelog
+  Changelog is now stored on GitHub
+已知問題 | Known Issues
+  Known issues is now stored on GitHub
+待辦事項 | Todos
+  Todo is now stored on GitHub
+著作權宣告 | Copyright notice
+  Copyright 2012 林博仁(Henry Lin, pika1021@gmail.com)
+智慧財產授權條款：
+  Test.cpp is part of CPP_Lab_7_1
+  CPP_Lab_7_1 is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  CPP_Lab_7_1 is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with CPP_Lab_7_1.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*--------------程式碼開始(Code Started)--------------*/
+/*--------------前期處理器指令(Preprocessor Directive)--------------*/
+/*////////程式所include之函式庫的標頭檔(Included Library Headers)////////*/
+/* */
+#include <cstdlib>
+#include <iostream>
+
+/* */
+#include "pauseProgram/Pause_program.h"
+
+/*////////常數與巨集(Constants & Macros)////////*/
+
+/*////////其他前期處理器指令(Other Preprocessor Directives////////*/
+
+/*--------------全域宣告與定義(Global Declaration & Definition)--------------*/
+/*////////Classes、資料結構(Data Structures)、type definitions跟enumerations////////*/
+
+/*////////函式雛型(Function Prototypes)////////*/
+
+/*////////全域變數(Global Variables)////////*/
+
+/*--------------主要程式碼(Main Code)--------------*/
+int main(int argc, char *argv[])
+{
+
+restart_program:
+
+
+
+
+  if(1 == pauseProgram()){
+    goto restart_program;
+  }
+  return EXIT_SUCCESS;
+}
